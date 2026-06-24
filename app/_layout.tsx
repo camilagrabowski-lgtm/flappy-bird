a9d49cfda0b32d55f2139dceb50c308036d82337
camilagrabowski-lgtm <camila.grabowski@aluno.senai.br>
2026-06-24 17:11:54 -0300

    Add files via upload

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,31 +1,34 @@
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { useEffect } from "react";
+import { Stack, ErrorBoundary } from "expo-router";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import "react-native-reanimated";
 
+import { LilitaOne_400Regular } from "@expo-google-fonts/lilita-one";
+import { LuckiestGuy_400Regular } from "@expo-google-fonts/luckiest-guy";
 
-export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
+export { ErrorBoundary };
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: 'index',
+  initialRouteName: "index",
 };
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
+// Impede que a Splash Screen desapareça automaticamente
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    LilitaOne: LilitaOne_400Regular,
+    LuckiestGuy: LuckiestGuy_400Regular,
+    ...FontAwesome.font,
   });
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
-    if (error) throw error;
+    if (error) {
+      throw error;
+    }
   }, [error]);
 
   useEffect(() => {
@@ -42,10 +45,10 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-
   return (
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-      </Stack>
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="play" />
+    </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,100 @@
-import { StyleSheet, Text} from "react-native";
+import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import MovingBackground from "@/components/MovingBackground";
 
 export default function Home() {
   return (
-  <SafeAreaView style={styles.screen}>
-    <Text style={styles.title}>Flappy Bird</Text>
-  </SafeAreaView>
+    <ImageBackground
+      source={require("../assets/images/background.png.avif")}
+      resizeMode="cover"
+      style={styles.background}
+    >
+      <SafeAreaView style={styles.screen}>
+        <Text style={styles.title}>
+          <Text style={styles.title}>FLAPPY{"\n"}</Text>
+          <Text style={styles.title}>BIRD{"\n"}</Text>
+        </Text>
+
+        <Link href="/play" asChild>
+          <TouchableOpacity style={styles.button}>
+            <LinearGradient
+              colors={["#FF8A00", "#FFD600"]}
+              style={styles.buttonGradient}
+            >
+              <Text style={styles.buttonText}>Jogar</Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </Link>
+        <Image
+          source={require("../assets/images/bird.webp")}
+          style={styles.bird}
+        />
+
+      </SafeAreaView>
+      <MovingBackground />
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+  },
+
   screen: {
-    width: "100%",
-    height: "100%",
-    backgroundColor: "black",
+    flex: 1,
+    justifyContent: "flex-start",
     alignItems: "center",
   },
+
+  button: {
+    position: "absolute",
+    top: "50%",
+    borderRadius: 100,
+    shadowColor: "black",
+    shadowOffset: { width: 1, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+  },
+
+  buttonGradient: {
+    paddingHorizontal: 40,
+    paddingVertical: 15,
+    borderRadius: 100,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  buttonText: {
+    color: "#fff",
+    fontSize: 20,
+    textShadowOffset: { width: 0, height: 2 },
+    textShadowRadius: 4,
+    textShadowColor: "black",
+    fontFamily: "LilitaOne",
+  },
+
   title: {
-    fontSize: 50,
-    fontWeight: "bold",
-    color: "white",
-    marginTop: 100,
-  }
+    marginTop: 10,
+    textAlign: "center",
+    fontSize: 100,
+    fontWeight: "900",
+    lineHeight: 100,
+  },
+  bird: {
+    width: 70,
+    height: 48,
+    position: "absolute",
+    top: "35%",
+    left: "35%",
+    transform: [{ rotate: "-20deg" }],
+  },
 });
